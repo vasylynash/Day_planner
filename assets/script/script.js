5883906521a6cb84d540594b113b9805f779fcad
Vasylyna Shevadutska <vaskash@gmail.com>
2021-04-21 12:23:44 -0700

    Added coloring logic

--- assets/script/script.js
+++ assets/script/script.js
@@ -12,17 +12,24 @@ table.append(tbody);
 container.append(table);
 
 for (let i = 0; i < 24; i++) {
-    if (i <= 12) {
+    if (i < 12) {
         tbody.append(`<tr class='row'> 
     <td class='col-1 hour'> <div>${i}AM</div></td> 
-    <td class='col-10 description'><div> </div></td> 
+    <td class='col-10 description' timestamp='${i}AM'><textarea> </textarea></td>
+    <td class='col-1'><input type='button' class='saveBtn'/><i>SAVE</i></td>
+    </tr>`)
+    }
+    else if (i === 12) {
+        tbody.append(`<tr class='row'> 
+    <td class='col-1 hour'> <div>${i}PM</div></td> 
+    <td class='col-10 description' timestamp='${i}PM'><textarea> </textarea></td>
     <td class='col-1'><input type='button' class='saveBtn'/><i>SAVE</i></td>
     </tr>`)
     }
     else {
         tbody.append(`<tr class='row'> 
     <td class='col-1 hour'> <div>${i - 12}PM</div></td> 
-    <td class='col-10 description'><div> </div></td> 
+    <td class='col-10 description' timestamp='${i - 12}PM'><textarea> </textarea></td>
     <td class='col-1'><input type='button' class='saveBtn'/><i>SAVE</i></td>
     </tr>`)
     }
@@ -30,19 +37,24 @@ for (let i = 0; i < 24; i++) {
 }
 
 var rows = $(".row");
-var hour = moment().format("hA");
+var hour = moment().format("hA"); // 3pm
 var times = $(".hour");
+var descriptions = $(".description");
 
-for (let i = 0; i < times.length; i++) {
-    const element = times[i];
-    if (element.innerText.trim() === hour) {
-        rows.addClass("present")
-    }
+descriptions.filter(function () {
+    // console.log($(this).attr("timestamp").toLowerCase());
+    return $(this).attr("timestamp") === hour;
+}).addClass("present");
 
-}
-// times.forEach(element => {
-//     if (element.val() === hour) {
-//         $(".description").addClass("present")
-//     }
-// });
+descriptions.filter(function () {
+    var time = moment($(this).attr("timestamp"), "hA")
+    // console.log(moment(hour, "hA"));
+    return time < moment(hour, "hA");
+}).addClass("past");
+
+descriptions.filter(function () {
+    var time = moment($(this).attr("timestamp"), "hA")
+    // console.log(moment(hour, "hA"));
+    return time > moment(hour, "hA");
+}).addClass("future");
 
