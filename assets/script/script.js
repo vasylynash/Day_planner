37e353b6f53c73dd2c8579c93621dd360e3c176f
Vasylyna Shevadutska <vaskash@gmail.com>
2021-04-22 17:07:39 -0700

    Reformatted script.js, added the support of multiple entries, cleaned up style.css

--- assets/script/script.js
+++ assets/script/script.js
@@ -12,28 +12,12 @@ table.append(tbody);
 container.append(table);
 
 for (let i = 0; i < 24; i++) {
-    if (i < 12) {
-        tbody.append(`<tr class='row'> 
-    <td class='col-1 hour' id="hour"> <div>${i}AM</div></td> 
-    <td class='col-10 description' timestamp='${i}AM' id="description"><textarea> </textarea></td>
-    <td class='col-1'><input type='button' class='saveBtn' id="save"/><i>SAVE</i></td>
-    </tr>`)
-    }
-    else if (i === 12) {
-        tbody.append(`<tr class='row'> 
-    <td class='col-1 hour' id="hour"> <div>${i}PM</div></td>
-    <td class='col-10 description' timestamp='${i}PM' id="description"><textarea> </textarea></td>
-    <td class='col-1'><input type='button' class='saveBtn' id="save"/><i>SAVE</i></td>
-    </tr>`)
-    }
-    else {
-        tbody.append(`<tr class='row'> 
-    <td class='col-1 hour' id="hour"> <div>${i - 12}PM</div></td>
-    <td class='col-10 description' timestamp='${i - 12}PM' id="description"><textarea> </textarea></td>
-    <td class='col-1'><input type='button' class='saveBtn' id="save"/><i>SAVE</i></td>
-    </tr>`)
-    }
-    ;
+    var time = moment().hour(i).format("hA");
+    tbody.append(`<tr class='row'> 
+    <td class='col-1 hour'> <div>${time}</div></td>
+    <td class='col-10 description' timestamp='${time}'><textarea> </textarea></td>
+    <td class='col-1'><input type='button' class='saveBtn'/><i>Save</i></td>
+    </tr>`);
 }
 
 var hour = moment().format("hA");
@@ -58,18 +42,32 @@ descriptions.filter(function () {
 
 buttons.on("click", function (event) {
     var entry = $(event.target).parent().siblings().eq(1).children().eq(0).val();
-    console.log(entry);
+    var processedEntry = entry.split("\n");
     var time = $(event.target).parent().siblings().eq(1).attr("timestamp");
-    localStorage.setItem(`${time}`, entry);
-    console.log(time);
+    localStorage.setItem(`${time}`, JSON.stringify(processedEntry));
 })
 
+function checkDateAndClearStorage() {
+    var date = localStorage.getItem("date");
+    var currentDate = moment().format("dddd, MMMM Do");
+    if (date !== currentDate) {
+        localStorage.clear();
+        localStorage.setItem("date", currentDate);
+    }
+}
 
-$(window).on("load", function () {
+function populateFromStorage() {
     for (let i = 0; i < times.length; i++) {
         var time = $(times)[i].innerText.trim();
-        var entry = localStorage.getItem(time);
-        var textarea = $("textarea");
-        $(descriptions).find(textarea)[i].innerText = entry;
+        if (JSON.parse(localStorage.getItem(time)) !== null) {
+            var entry = JSON.parse(localStorage.getItem(time)).join("\n");
+            var textarea = $("textarea");
+            $(descriptions).find(textarea)[i].innerHTML = entry;
+        }
     }
+}
+
+$(window).on("load", function () {
+    checkDateAndClearStorage();
+    populateFromStorage();
 })
